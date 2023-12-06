1977d80ceca1cc8a5ea0cde4956cf88333378cd1
Felipe Messa <51674627+messafelipe@users.noreply.github.com>
2023-12-05 21:26:57 -0300

    Adição do ProjectStatusEnum, IProjectRepository e ProjectAlreadyStartedException.

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStartedException : Exception
+    {
+        public ProjectAlreadyStartedException() : base("Project is already is Started status")
+        {
+
+        }
+    }
+}
