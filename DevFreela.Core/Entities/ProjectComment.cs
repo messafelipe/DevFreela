962a0f01ea8816c415136cbde75266d5da8c91e7
Felipe Messa <51674627+messafelipe@users.noreply.github.com>
2023-12-05 21:25:12 -0300

    Criação das Entidades na Camada Core.

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,17 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreateAt { get; private set; }
+    }
+}
