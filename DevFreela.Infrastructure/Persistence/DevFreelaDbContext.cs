f6d78b2b7e8b6bf23295375a598205c04b378f04
Felipe Messa <51674627+messafelipe@users.noreply.github.com>
2024-05-19 10:45:25 -0300

    Adição dos Pacotes EntityFrameworkCore.SqlServer e EntityFrameworkCore.Tools. Alteração do DevFreelaDbContext para aceitar o DbContext e DbSet.

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,33 +1,23 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options) 
         {
-            Projects = new List<Project>
-            {
-                new Project("Meu projeto ASPNET Core 1", "Minha descrição de Projeto 1", 1, 1, 1000)
-            };
 
-            Users = new List<User>
-            {
-                new User("Luis Felipe", "luisdev@luisdev.com.br", new DateTime(1992, 1, 1))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core")
-            };
         }
 
-        public List<Project> Projects { get; set; }
+        public DbSet<Project> Projects { get; set; }
 
-        public List<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
-        public List<Skill> Skills { get; set; }
+        public DbSet<Skill> Skills { get; set; }
 
-        public List <ProjectComment> ProjectComments { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
 }
